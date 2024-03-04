e1ff578a73786df95e292d67e3e2cec3136a8091
Евтифеева Милана Анатольевна <EvtifeevaMA@DOM201.LOCAL>
2024-03-04 10:05:05 +0700

    add task6

--- Lab 3/Часть 2/Task6/Task.js
+++ Lab 3/Часть 2/Task6/Task.js
@@ -1,16 +1,34 @@
 /*Написать функцию, которая после вызова каждую секунду пишет в консоль очередное число Фибоначчи. Так, в консоли будет: 0, 1, 1, 2, 3, 5, 8, 13...*/
-
-function Fib(n)
-{
-    console.log(n);
-    if(n<=1)
-    {
-       return n;
-    }
-    setTimeout(Fib,1000,Fib(n-1)+Fib(n-2));
-  
+let i=0;
+function fibNumbers()
+ {
     
+    let a = 0;
+    let b = 1;
+
+    function fibonacci ()
+    {
+        if(i==0)
+        {
+            console.log(a)
+            i++
+        }
+        else if(i==1)
+        {
+            console.log(b)
+            i++ 
+        }
+
+        else{
+            let next = a + b;
+            console.log(next);
+            a = b;
+            b = next;
+        }
+
+    };
+
+    setInterval(fibonacci, 1000);
 }
 
-Fib(10);
-
+fibNumbers();
