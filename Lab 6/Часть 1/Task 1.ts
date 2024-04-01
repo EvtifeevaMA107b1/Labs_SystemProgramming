7238acc9ee681e1bc7a5cd4f88ce1367b3c1c344
Евтифеева Милана Анатольевна <EvtifeevaMA@DOM201.LOCAL>
2024-04-01 09:32:03 +0700

    Lab 6

---
+++ Lab 6/Часть 1/Task 1.ts
@@ -0,0 +1,49 @@
+/*Избавиться от unknown, описать актуальный интерфейс:
+
+export type User = unknown;
+
+export const users: unknown[] = [
+    {
+        name: 'Max Mustermann',
+        age: 25,
+        occupation: 'Chimney sweep'
+    },
+    {
+        name: 'Kate Müller',
+        age: 23,
+        occupation: 'Astronaut'
+    }
+];
+
+export function logPerson(user: unknown) {
+    console.log(` - ${user.name}, ${user.age}`);
+}
+
+console.log('Users:');
+users.forEach(logPerson);*/
+
+interface User{
+    name: string;
+    age: number;
+    occupation: string;
+}
+
+const users: User[] = [
+    {
+        name: 'Max Mustermann',
+        age: 25,
+        occupation: 'Chimney sweep'
+    },
+    {
+        name: 'Kate Müller',
+        age: 23,
+        occupation: 'Astronaut'
+    }
+];
+
+function logPerson(user: User) {
+    console.log(` - ${user.name}, ${user.age}`);
+}
+
+console.log('Users:');
+users.forEach(logPerson);
