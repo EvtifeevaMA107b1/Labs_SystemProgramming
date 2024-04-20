63b0a15e945fb322417d5a60ca000f386326d9af
Milana Evtifeeva <evtifeevama@mer.ci.nsu.ru>
2024-04-20 22:24:15 +0700

    1 7

---
+++ Lab 7/applab7/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Square from './Components/Square'
+function App() {
+  return (
+    <div className="App">
+    
+
+        <div>
+          <h2>Task1</h2>      
+            <Square n={3}/>
+        </div>
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ Lab 7/applab7/src/Components/Square.js
@@ -0,0 +1,8 @@
+function Square({n})
+{
+    const sq=n*n;
+    return(
+        <p>{sq}</p>
+    );
+}
+export default Square
