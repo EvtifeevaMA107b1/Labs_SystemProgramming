3f974b508d944a7284706cd93a4efe23acb04274
Milana Evtifeeva <evtifeevama@mer.ci.nsu.ru>
2024-04-21 18:29:53 +0700

    8 1

---
+++ Lab 8/applab8/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import ReTimer from './Components/ReTimer';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <h2>Task1</h2>
+        <ReTimer/>
+      </div>
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ Lab 8/applab8/src/Components/ReTimer.js
@@ -0,0 +1,25 @@
+import  { useState } from 'react';
+function ReTimer()
+{    const [isOn, setisOn] = useState(true);
+    const [count, setCount] = useState(10);
+
+    const timer=()=>
+    {
+    if (count > 0 & isOn) {
+        setCount(count - 1);
+    }
+      else
+      {
+        setisOn(false);
+        
+      }};
+  setTimeout(timer,1000)
+    // Запускать countdown каждую секунду
+    
+   
+    return(
+        <p>{count}</p>
+       
+    );
+}
+export default ReTimer
